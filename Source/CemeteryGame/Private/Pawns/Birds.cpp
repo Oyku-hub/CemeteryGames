80726719a87aebcc0b8ee663a767ed20dfb50e90
Oyku Ece Koken <oecekoken@gmail.com>
2024-01-26 12:43:51 +0300

    sadsad

--- Source/CemeteryGame/Private/Pawns/Birds.cpp
+++ Source/CemeteryGame/Private/Pawns/Birds.cpp
@@ -15,6 +15,8 @@ ABirds::ABirds()
 	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
 	BirdMesh->SetupAttachment(GetRootComponent());
 
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
@@ -23,7 +25,6 @@ void ABirds::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
 // Called every frame
 void ABirds::Tick(float DeltaTime)
 {
@@ -35,6 +36,23 @@ void ABirds::Tick(float DeltaTime)
 void ABirds::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ABirds::MoveForward);
+
+
+
+}
+
+void ABirds::MoveForward(float Value)
+{
+
+	if (Controller && (Value != 0.f))
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, Value);
+
+	}
+	 
+
 
 }
 
--- Source/CemeteryGame/Public/Pawns/Birds.h
+++ Source/CemeteryGame/Public/Pawns/Birds.h
@@ -30,7 +30,7 @@ private:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	void MoveForward(float Value);
 
 
 };
